cb683dbed2a9be18ae6477004f80afedf7719e2d
Nguyen Van Ngan <ngan16021997ngan.gmail.com>
2017-10-12 16:02:43 +0700

    bien php

---
+++ bien.php
@@ -0,0 +1,13 @@
+<?php
+    define("PI", 3.14);
+    define('HOTGIRL', 'Gai Dep');
+
+    $name = "Nguyen Van Ngan";
+    $address = "Hue";
+    $old = 20;
+    echo "Ten: $name <br>";
+    echo "Dia chi: $address <br>";
+    echo "Tuoi: $old <br>";
+    echo HOTGIRL;
+    echo PI;
+?>
