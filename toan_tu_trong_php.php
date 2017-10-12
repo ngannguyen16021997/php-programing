2366db5fca9b07e9349ab9d4207e12875297dfad
Nguyen Van Ngan <ngan16021997ngan.gmail.com>
2017-10-12 16:41:26 +0700

    toan_tu_php

---
+++ toan_tu_trong_php.php
@@ -0,0 +1,18 @@
+<?php
+    $a = 5;
+    $b =4;
+    $tong = $a + $b;
+    echo "$a + $b = $tong <br>";
+    $hieu = $a - $b;
+    echo "$a - $b = $hieu <br>";
+    $tich = $a * $b;
+    echo "$a * $b = $tich <br>";
+    $thuong = $a / $b;
+    echo "$a / $b = $thuong <br>";
+    $sodu = $a % $b;
+    echo "$a % $b = $sodu <br>";
+    $atang = ++$a;
+    echo "a++ = $atang";
+    $agiam = --$a;
+    echo "a-- = $agiam";
+?>
